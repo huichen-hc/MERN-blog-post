4ad9c6b3708ca5d2aeed7bb1e539f701ad06b813
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2025-04-18 10:52:59 +0200

    Add css to CreateBlog form

--- frontend/src/pages/CreateBlog.jsx
+++ frontend/src/pages/CreateBlog.jsx
@@ -7,9 +7,7 @@ function CreateBlog() {
   const [description, setDescription] = useState("");
   const [content, setContent] = useState("");
 
-
   async function handleSubmit() {
-
     let submitObject = {
       title: title,
       description: description,
@@ -23,32 +21,42 @@ function CreateBlog() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Blog Post Title</label>
+    <>
+    <h1>Create a new post </h1>
+    <form className="create-blog-form" onSubmit={handleSubmit}>
+      <label htmlFor="title">Blog Post Title</label>
       <input
         type="text"
+        id="title"
+        placeholder="Enter the title of your blog post"
         onChange={(e) => setTitle(e.target.value)}
         maxLength={100}
         required
         name="title"
       />
-      <label>Blog Post Description</label>
+      <label htmlFor="description">Blog Post Description</label>
       <input
         type="text"
+        id="description"
+        placeholder="Enter a short description of your blog post"
         onChange={(e) => setDescription(e.target.value)}
         maxLength={200}
         required
         name="description"
       />
-      <label>Blog Post Content</label>
+      <label htmlFor="content">Blog Post Content</label>
       <textarea
+        id="content"
+        placeholder="Write your blog post content here..."
         onChange={(e) => setContent(e.target.value)}
         maxLength={5000}
         required
         name="content"
+        rows="15"
       />
       <button type="submit">Submit</button>
     </form>
+    </>
   );
 }
 
