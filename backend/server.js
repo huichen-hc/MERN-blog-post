feec937d99e4f6b4c50c644b1ea179af5c4d7798
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2025-05-08 22:04:11 +0200

    chore: prepare server for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const posts = require("./postRoutes");
 const users = require("./userRoutes");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
